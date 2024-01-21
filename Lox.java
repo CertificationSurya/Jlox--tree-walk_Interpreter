944bbd8a689973a9c38f098e89bd87f81db23abd
certificationSurya - Pc <certification.surya@gmail.com>
2024-01-22 00:03:29 +0545

    feat: creating tree-walk interpreter -> jlox

---
+++ Lox.java
@@ -0,0 +1,73 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+// import java.util.Scanner;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox[script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    // for running from command line when file path is given;
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code
+        if (hadError)
+            System.exit(65);
+    }
+
+    // To run jlox from command line
+    public static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+            hadError = false; // added to not kill entire session.
+        }
+    }
+
+    // for scanner.
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // perform scanner wise operation
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    // Error Handling
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+}
